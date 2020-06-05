63c52d31a3be949275d2a5462f58d043cedaa1e0
sumitup80 <sumitup80@gmail.com>
2020-06-05 17:18:16 +0530

    Removed Application properties to test config map and also removed eureka settings

--- APIGatewayDemo/src/main/java/com/example/demo/ApiGatewayDemoApplication.java
+++ APIGatewayDemo/src/main/java/com/example/demo/ApiGatewayDemoApplication.java
@@ -2,10 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class ApiGatewayDemoApplication {
 
 	public static void main(String[] args) {
